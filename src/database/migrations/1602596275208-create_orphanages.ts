b9a5e26e6a37127ccc46eabb9b1f40b7277e0818
Benício Barreto <benicio.daniel@gmail.com>
2020-10-13 11:07:56 -0300

    Reverted the migration to add a column

--- src/database/migrations/1602596275208-create_orphanages.ts
+++ src/database/migrations/1602596275208-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602596275208 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
